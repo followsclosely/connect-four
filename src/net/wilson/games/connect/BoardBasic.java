0cd2f2f30bad4c9df449808be930b07bb28a7cf1
Matthew Wilson <mlavwilson@gmail.com>
2021-01-30 21:02:04 -0500

    Moved getWinner to engine.

--- src/net/wilson/games/connect/BoardBasic.java
+++ src/net/wilson/games/connect/BoardBasic.java
@@ -4,7 +4,6 @@ import java.awt.geom.Dimension2D;
 
 public class BoardBasic implements Board {
     int state[][] = new int[7][6];
-    int last[] = new int[2];
 
     public BoardBasic(){
     }
@@ -24,64 +23,20 @@ public class BoardBasic implements Board {
         return (state[x][0] == 0);
     }
 
-    public boolean dropPiece(int x, int piece)
+    public int dropPiece(int x, int piece)
     {
         int y = 5;
         for(; y>=0; y--){
             if( getPiece(x,y) == 0){
-                state[last[0] = x][last[1] = y] = piece;
-                return true;
+                state[x][y] = piece;
+                return y;
             }
         }
 
-        return false;
+        return -1;
     }
 
-    public boolean getWinner() {
 
-        boolean winner = false;
-
-        int x = last[0], y = last[1];
-        int color = state[x][y];
-
-        // Horizontal First
-        int horizontalCount = 1;
-        for (int i=1; i<4 && x-i >= 0 && state[x-i][y] == color; i++, horizontalCount++);
-        for (int i=1; i<4 && x+i < getWidth() && state[x+i][y] == color; i++, horizontalCount++);
-        if( horizontalCount >= 4) {
-            winner = true;
-            System.out.println("We have a horizontal winner ("+color+") ["+x+","+y+"]!");
-        }
-
-        // Vertical
-        int verticalCount = 1;
-        for (int i=1; i<4 && y-i >= 0 && state[x][y-i] == color; i++, verticalCount++);
-        for (int i=1; i<4 && y+i < getHeight() && state[x][y+i] == color; i++, verticalCount++);
-        if( verticalCount >= 4) {
-            winner = true;
-            System.out.println("We have a vertical winner ("+color+") ["+x+","+y+"]!");
-        }
-
-        // Forward Slash Diagonal /
-        int forwardSlashCount = 1;
-        for (int i=1; i<4 && y+i < getHeight() && x-i >= 0         && state[x-i][y+i] == color; i++, forwardSlashCount++);
-        for (int i=1; i<4 && y-i >=0           && x+i < getWidth() && state[x+i][y-i] == color; i++, forwardSlashCount++);
-        if( forwardSlashCount >= 4) {
-            winner = true;
-            System.out.println("We have a / winner ("+color+") ["+x+","+y+"]!");
-        }
-
-        //Back Slash Diagonal \
-        int backSlashCount = 1;
-        for (int i=1; i<4 && y-i >= 0          && x-i >= 0         && state[x-i][y-i] == color; i++, backSlashCount++);
-        for (int i=1; i<4 && y+i < getHeight() && x+i < getWidth() && state[x+i][y+i] == color; i++, backSlashCount++);
-        if( backSlashCount >= 4) {
-            winner = true;
-            System.out.println("We have a \\ winner ("+color+") ["+x+","+y+"]!");
-        }
-
-        return winner;
-    }
 
     public String toMatrixString() {
         StringBuffer b = new StringBuffer();
--- src/net/wilson/games/connect/Engine.java
+++ src/net/wilson/games/connect/Engine.java
@@ -23,13 +23,59 @@ public class Engine {
             int x = player.yourTurn(board);
             if( x >= 0 && x < board.getWidth() ) {
                 int color = (turn%2) + 1;
-                board.dropPiece(x, color);
+                int y = board.dropPiece(x, color);
                 System.out.println(board);
-                if ( board.getWinner() )
+                if ( getWinner(x,y) )
                 {
                     return;
                 }
             }
         }
+    }
+
+    public boolean getWinner(int x, int y) {
+
+        boolean winner = false;
+
+        //int x = last[0], y = last[1];
+        int color = board.getPiece(x,y);
+
+        // Horizontal First
+        int horizontalCount = 1;
+        for (int i=1; i<4 && x-i >= 0 && board.getPiece(x-i,y) == color; i++, horizontalCount++);
+        for (int i=1; i<4 && x+i < board.getWidth() && board.getPiece(x+i,y) == color; i++, horizontalCount++);
+        if( horizontalCount >= 4) {
+            winner = true;
+            System.out.println("We have a horizontal winner ("+color+") ["+x+","+y+"]!");
+        }
+
+        // Vertical
+        int verticalCount = 1;
+        for (int i=1; i<4 && y-i >= 0 && board.getPiece(x,y-i) == color; i++, verticalCount++);
+        for (int i=1; i<4 && y+i < board.getHeight() && board.getPiece(x,y+i) == color; i++, verticalCount++);
+        if( verticalCount >= 4) {
+            winner = true;
+            System.out.println("We have a vertical winner ("+color+") ["+x+","+y+"]!");
+        }
+
+        // Forward Slash Diagonal /
+        int forwardSlashCount = 1;
+        for (int i=1; i<4 && y+i < board.getHeight() && x-i >= 0         && board.getPiece(x-i,y+i) == color; i++, forwardSlashCount++);
+        for (int i=1; i<4 && y-i >=0           && x+i < board.getWidth() && board.getPiece(x+i,y-i) == color; i++, forwardSlashCount++);
+        if( forwardSlashCount >= 4) {
+            winner = true;
+            System.out.println("We have a / winner ("+color+") ["+x+","+y+"]!");
+        }
+
+        //Back Slash Diagonal \
+        int backSlashCount = 1;
+        for (int i=1; i<4 && y-i >= 0          && x-i >= 0         && board.getPiece(x-i,y-i) == color; i++, backSlashCount++);
+        for (int i=1; i<4 && y+i < board.getHeight() && x+i < board.getWidth() && board.getPiece(x+i,y+i) == color; i++, backSlashCount++);
+        if( backSlashCount >= 4) {
+            winner = true;
+            System.out.println("We have a \\ winner ("+color+") ["+x+","+y+"]!");
+        }
+
+        return winner;
     }
 }

5b8ea1d932753a4cc7079ef09a87ca430f5f5bbe
Matthew Wilson <mlavwilson@gmail.com>
2021-02-24 22:55:06 -0500

    Added setters

--- entries/followsclosely/src/main/java/io/github/followsclosley/connect/ai/Config.java
+++ entries/followsclosely/src/main/java/io/github/followsclosley/connect/ai/Config.java
@@ -12,22 +12,56 @@ public class Config {
     public int getWinner() {
         return winner;
     }
-    public int getCenter() {
-        return center;
+
+    public void setWinner(int winner) {
+        this.winner = winner;
     }
-    public int getYourColorInRow() {
-        return yourColorInRow;
-    }
-    public int getEmptyInRow() {
-        return emptyInRow;
-    }
+
     public int getLooser() {
         return looser;
     }
+
+    public void setLooser(int looser) {
+        this.looser = looser;
+    }
+
     public int getLooserInTwo() {
         return looserInTwo;
     }
+
+    public void setLooserInTwo(int looserInTwo) {
+        this.looserInTwo = looserInTwo;
+    }
+
     public int getLooserInOne() {
         return looserInOne;
+    }
+
+    public void setLooserInOne(int looserInOne) {
+        this.looserInOne = looserInOne;
+    }
+
+    public int getCenter() {
+        return center;
+    }
+
+    public void setCenter(int center) {
+        this.center = center;
+    }
+
+    public int getYourColorInRow() {
+        return yourColorInRow;
+    }
+
+    public void setYourColorInRow(int yourColorInRow) {
+        this.yourColorInRow = yourColorInRow;
+    }
+
+    public int getEmptyInRow() {
+        return emptyInRow;
+    }
+
+    public void setEmptyInRow(int emptyInRow) {
+        this.emptyInRow = emptyInRow;
     }
 }

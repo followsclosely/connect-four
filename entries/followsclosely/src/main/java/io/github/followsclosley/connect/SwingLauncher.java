b39b5c31cb505e65c025e56169fc63ba01db94da
Matthew Wilson <mlavwilson@gmail.com>
2021-02-27 18:13:47 -0500

    Added PIECE_SIZE var.

--- entries/followsclosely/src/main/java/io/github/followsclosley/connect/SwingLauncher.java
+++ entries/followsclosely/src/main/java/io/github/followsclosley/connect/SwingLauncher.java
@@ -1,6 +1,7 @@
 package io.github.followsclosley.connect;
 
 import io.github.followsclosley.connect.ai.ScoreStrategy;
+import io.github.followsclosley.connect.impl.MutableBoard;
 import io.github.followsclosley.connect.swing.SwingSupport;
 
 /**
@@ -9,7 +10,7 @@ import io.github.followsclosley.connect.swing.SwingSupport;
 public class SwingLauncher {
     public static void main(String[] args) {
         new SwingSupport()
-                //.setBoard(new MutableBoard(7,6,4))
+                //.setBoard(new MutableBoard(11,15,4))
                 //.setArtificialIntelligence(new Dummy(SwingSupport.COMPUTER_COLOR))
                 .setArtificialIntelligence(new ScoreStrategy(SwingSupport.COMPUTER_COLOR, SwingSupport.PLAYER_COLOR))
                 //.setArtificialIntelligence(new MiniMaxAlgorithm(SwingSupport.COMPUTER_COLOR, SwingSupport.PLAYER_COLOR, SwingSupport.COMPUTER_COLOR))

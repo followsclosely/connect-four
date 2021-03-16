b0460203686dc1ee48c241977116bfa7d803843c
Matthew Wilson <mlavwilson@gmail.com>
2021-03-15 21:55:04 -0400

    replaced jaron with dummy

--- entries/lane/src/main/java/io/github/lane/Main.java
+++ entries/lane/src/main/java/io/github/lane/Main.java
@@ -1,12 +1,13 @@
 package io.github.lane;
 
 import io.github.followsclosley.connect.Simulation;
+import io.github.followsclosley.connect.impl.ai.Dummy;
 
 public class Main {
     public static void main(String[] args) {
         new Simulation()
                 .number(1000)
-                .addArtificialIntelligence(new JaronBot(2))
+                .addArtificialIntelligence(new Dummy(2))
                 .addArtificialIntelligence(new LaneAI(1, 2, 10))
                 .run()
                 .printSummary();

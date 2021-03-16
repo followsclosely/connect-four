7b09db82cd835eed82f509656a8a1d6140f65e5b
Ryan Pelletier <ryanmatthewpelletier@gmail.com>
2021-03-15 21:48:14 -0400

    initial commit

---
+++ entries/ryanp/src/main/java/ShellLauncher.java
@@ -0,0 +1,13 @@
+import io.github.followsclosley.connect.Simulation;
+import io.github.followsclosley.connect.impl.ai.Dummy;
+
+public class ShellLauncher {
+    public static void main(String[] args) {
+        new Simulation()
+                .number(200000)
+                .addArtificialIntelligence(new Dummy(1))
+                .addArtificialIntelligence(new RyanPAI(2))
+                .run()
+                .printSummary();
+    }
+}
---
+++ entries/ryanp/src/main/java/SwingLauncher.java
@@ -0,0 +1,14 @@
+import io.github.followsclosley.connect.impl.MutableBoard;
+import io.github.followsclosley.connect.swing.SwingSupport;
+
+/**
+ * Class to launch a Swing interface to test your AI.
+ */
+public class SwingLauncher {
+    public static void main(String[] args) {
+        new SwingSupport()
+                .setBoard(new MutableBoard())
+                .setArtificialIntelligence(new RyanPAI(SwingSupport.COMPUTER_COLOR))
+                .run();
+    }
+}
